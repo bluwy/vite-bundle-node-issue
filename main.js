57b117b475b03b57524d28a2415894ab8f617ea7
bluwy <bjornlu.dev@gmail.com>
2022-05-04 02:49:48 +0800

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import 'decoders'
+
+if (false) {
+  // UNCOMMENT THE FOLLOWING LINE TO TRIGGER THE BUG
+  import('@sentry/node')
+}
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
