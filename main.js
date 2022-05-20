b87fec61b1dc9301a0a4726633a2f9fd45f44106
bluwy <bjornlu.dev@gmail.com>
2022-05-20 13:52:49 +0800

    add dblclick example

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 import './style.css'
-import 'decoders'
 
+import('decoders')
 if (false) {
   // UNCOMMENT THE FOLLOWING LINE TO TRIGGER THE BUG
-  import('@sentry/node')
+  // import('@sentry/node')
 }
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+
+document.addEventListener('dblclick', () => {
+  import('@sentry/node')
+})
